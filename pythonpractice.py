8e080f8b33160c0749de68df4879e804972f7fc2
ANitkin8 <ah.nitkin@gmail.com>
2026-08-23 18:20:53 +0300

    Add files via upload

---
+++ pythonpractice.py
@@ -0,0 +1,47 @@
+##exercise 1
+print("hello world \n hello world \n hello world \n hello world")
+##exercise 2
+a=(99^3)*8
+print(a)
+##exercise 3
+## 5 < 3 false
+## 3 == 3 true
+## 3 == "3" false
+## "3" > 3 false
+## "Hello" == "hello" false
+
+##exercise 4
+computer_brand = "Dell"
+print (f"I have a {computer_brand} computer")
+##exercise 5
+name= "Aharon"
+age= 43
+shoe_size= 46
+info = "I play guitar"
+print(info)
+##exercise 6
+a= 5
+b= 3
+if a>b:
+    print("hello world")
+##exercise 7
+print("enter a number")
+number= int(input())
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+#exercise 8
+print("whats your name?")
+user_name= input()
+if user_name =="Aharon":
+    print("what a great name!")
+else:
+    print("where did you get a stupid name like that?")
+# exercise 8
+print("how tall are you?")
+height=int(input())
+if height >145:
+    print("you are tall enough to ride")
+else:
+    print("need to grow more")
